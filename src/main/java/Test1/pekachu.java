0fa8db4a075bc5f61f9306d132e6166297cce1e5
HuJiaKai1224 <0710403@go.hwh.edu.tw>
2020-10-23 10:52:53 +0800

    New package! BMI

---
+++ src/main/java/Test1/pekachu.java
@@ -0,0 +1,11 @@
+package Test1;
+
+public class pekachu {
+    public static void main(String[] args) {
+        jnejne a= new jnejne(80.5f,1.7f);
+        a.height=1.7f;
+        a.weight=80.5f;
+        System.out.println(a.bmi());
+    }
+
+}
